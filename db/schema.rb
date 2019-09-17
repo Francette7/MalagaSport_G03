68b15c8b467782862cdf2ba4ba5015622f4c39f5
danis1996 <danisandrios@gmail.com>
2019-09-17 21:21:45 +0300

    save avatar and static page

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_09_17_114243) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
